d9061dbb2f49640760b61fe3da0ac1d6bb1ffd5b
Rodrigo-M-Brazao <rodrigombrazao@hotmail.com>
2023-03-27 18:35:23 -0300

    começo do login

---
+++ Foxtrot/app/Http/Controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'USUARIO_EMAIL' => ['required', 'email'],
+            'USUARIO_SENHA' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
