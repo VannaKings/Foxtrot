dbf698244074bf76081a99046c5fa33bb7b5e269
Geovanna Reis <101362358+VannaKings@users.noreply.github.com>
2023-03-27 20:54:32 -0300

    Página perfil - layout

--- Foxtrot/resources/views/home/perfil.blade.php
+++ Foxtrot/resources/views/home/perfil.blade.php
@@ -22,7 +22,7 @@
     <script src="https://kit.fontawesome.com/0169495cc4.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<div id="header">
+    <div id="header">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
             <a class="navbar-brand" href="/" style="max-width: 55px;"><img src="/images/logo-abreviada.png" alt="" style="max-width: 100px; max-height: 50px;"></a>
@@ -35,13 +35,13 @@
                 <i class="fa-solid fa-bars"></i>Menu
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="/">Inicio</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Bonecas</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Carrinhos</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Colecionaveis</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Lego</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Esportes</a></li>
-                    <li><a class="dropdown-item" href="/produtos">Bebês</a></li>
+                <li><a class="dropdown-item" href="/">Inicio</a></li>
+                <li><a class="dropdown-item" href="/produtos">Bonecas</a></li>
+                <li><a class="dropdown-item" href="/produtos">Carrinhos</a></li>
+                <li><a class="dropdown-item" href="/produtos">Colecionaveis</a></li>
+                <li><a class="dropdown-item" href="/produtos">Lego</a></li>
+                <li><a class="dropdown-item" href="/produtos">Esportes</a></li>
+                <li><a class="dropdown-item" href="/produtos">Bebês</a></li>
                 </ul>
             </div>
 
@@ -52,24 +52,39 @@
 
             <div class="dropdown">
                 <button type="button" class="btn btn-primary dropdown botao" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                <i class="fa-solid fa-user"></i>Olá, fulano
+                </button>
+
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/">Perfil</a></li>
+                <li><a class="dropdown-item" href="/produtos">Pedidos</a></li>
+                <li><a class="dropdown-item" href="/produtos">Carrinho</a></li>
+                </ul>
+            </div>
+
+            <!-- <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown botao" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
                 <i class="fa-solid fa-user"></i>Olá! entre ou cadastre-se
                 </button>
                 <div class="dropdown-menu">
                 <form class=" px-3 py-3">
                     <div class="mb-3">
-                        <label for="exampleDropdownFormEmail2" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+                    <label for="exampleDropdownFormEmail2" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleDropdownFormPassword2" class="form-label">Senha</label>
-                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+                    <label for="exampleDropdownFormPassword2" class="form-label">Senha</label>
+                    <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+                    </div>
+                    <div class="mb-3">
+
                     </div>
                     <button type="submit" class="btn btn-primary">Entrar</button>
                 </form>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="/cadastro/index.html">Novo por aqui? Cadastre-se</a>
+                <a class="dropdown-item" href="/cadastro">Novo por aqui? Cadastre-se</a>
                 </div>
-            </div>
+            </div> -->
 
             <div class="carrinho">
                 <i class="fa-solid fa-cart-shopping"></i>
@@ -79,57 +94,95 @@
         </nav>
     </div>
 
+    <!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark"> -->
+        <!-- Logo -->
+        <!-- <a class="navbar-brand" href="#">Minha Loja</a> -->
+        <!-- Links -->
+        <!-- <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Perfil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Histórico de Pedidos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Endereço</a>
+        </li>
+        </ul>
+    </nav> -->
     <div class="espaco"></div>
-
     <div class="container">
-
+        <!-- <h2>Perfil do Usuário</h2>
         <div class="row">
-            <div class="col-md-3 menu">
-                <h3 style="text-align:center;">Olá, Fulano</h3>
-                <!-- Nav seções -->
-                <ul class="nav-vertical">
-                    <li><i class="fa-solid fa-user"></i><a class="filtro" href= "">Dados pessoais</a></li>
-                    <li><i class="fa-solid fa-gift"></i><a class="filtro"  href= "">Pedidos</a></li>
-                    <li><i class="fa-solid fa-cart-shopping" style="font-size:15px"></i><a class="filtro" href= "">Carrinho</a></li>
-                    <li><i class="fa-solid fa-location-dot"></i><a class="filtro" href= "">Endereços</a></li>
-                </ul>
-                <!-- <div class="mt-4">
-                    <h4 class="mb-4">Endereços Cadastrados</h4>
-                    <ul class="list-group">
-                    <li class="list-group-item">Rua A, Número B, Bairro C</li>
-                    <li class="list-group-item">Rua D, Número E, Bairro F</li>
-                    </ul>
-                    <a href="#" class="btn btn-primary mt-3">Cadastrar Novo Endereço</a>
-                </div> -->
+            <div class="col-md-6">
+                <h3>Informações Pessoais</h3>
+                <p>Nome: João Silva</p>
+                <p>E-mail: joaosilva@gmail.com</p>
+                <p>Telefone: (11) 99999-9999</p>
             </div>
-            <div class="col-md-9">
-                <h4 class="mb-4">Histórico de Pedidos</h4>
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>ID do Pedido</th>
-                        <th>Data do Pedido</th>
-                        <th>Status do Pedido</th>
-                        <th>Valor Total</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>001</td>
-                        <td>01/01/2022</td>
-                        <td>Pago</td>
-                        <td>R$ 100,00</td>
-                    </tr>
-                    <tr>
-                        <td>002</td>
-                        <td>02/02/2022</td>
-                        <td>Enviado</td>
-                        <td>R$ 50,00</td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div class="col-md-6">
+                <h3>Endereço</h3>
+                <p>Rua A, 123</p>
+                <p>Bairro: Centro</p>
+                <p>Cidade: São Paulo</p>
+                <p>CEP: 01234-567</p>
             </div>
         </div>
+        <hr> -->
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Informações Pessoais</h3>
+                <p><strong>Nome:</strong> João Silva</p>
+                <p><strong>E-mail:</strong> joaosilva@gmail.com</p>
+                <p><strong>Telefone:</strong> (11) 99999-9999</p>
+                <p><strong>CPF:</strong> 123.456.789-00</p>
+                <p><strong>Data de Nascimento:</strong> 01/01/1980</p>
+            </div>
+            <div class="col-md-4">
+                <h3>Endereço de Entrega</h3>
+                <p><strong>Rua:</strong> A, 123</p>
+                <p><strong>Bairro:</strong> Centro</p>
+                <p><strong>Cidade:</strong> São Paulo</p>
+                <p><strong>CEP:</strong> 01234-567</p>
+                <p><strong>País:</strong> Brasil</p>
+            </div>
+            <div class="col-md-4">
+                <h3>Endereço de Cobrança</h3>
+                <p><strong>Rua:</strong> B, 456</p>
+                <p><strong>Bairro:</strong> Centro</p>
+                <p><strong>Cidade:</strong> São Paulo</p>
+                <p><strong>CEP:</strong> 01234-567</p>
+                <p><strong>País:</strong> Brasil</p>
+            </div>
+        </div>
+
+        <h3>Histórico de Pedidos</h3>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID do Pedido</th>
+                    <th>Data</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>001</td>
+                    <td>01/01/2023</td>
+                    <td>R$ 100,00</td>
+                </tr>
+                <tr>
+                    <td>002</td>
+                    <td>02/01/2023</td>
+                    <td>R$ 200,00</td>
+                </tr>
+                <tr>
+                    <td>003</td>
+                    <td>03/01/2023</td>
+                    <td>R$ 300,00</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
